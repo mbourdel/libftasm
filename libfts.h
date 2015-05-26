038396bc7aadec71cd5011bc624cab03570ec184
mbourdel <mlebourdelles@free.fr>
2015-05-26 18:03:40 +0200

    v0.4

---
+++ libfts.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libfts.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbourdel <mbourdel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/26 11:01:46 by mbourdel          #+#    #+#             */
+/*   Updated: 2015/05/26 11:04:01 by mbourdel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTS_H
+# define LIBFTS_H
+
+void			ft_bzero(void *s, size_t size);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				ft_isalnum(int c);
+int				ft_tolower(int c);
+int				ft_toupper(int c);
+char			*ft_strcat(char *dest, const char *src);
+
+#endif
