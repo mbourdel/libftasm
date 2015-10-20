c2a6a0cfdb27ddbf3591b0ce4becd20a0c3cd354
Matthieu BOURDELLES <mbourdel@e3r6p4.42.fr>
2015-10-20 18:45:30 +0200

    up cat

--- ft_cat.s
+++ ft_cat.s
@@ -7,55 +7,58 @@
 global _ft_cat
 
 section .bss
+
 buffer:
 	.buf resb SIZE
 
 section .text
-_ft_cat:
-	push rbp
-	mov rbp, rsp
 
-init_data:
-	; RDI contient le file descriptor
-	mov rdx, SIZE ; size dans rdx (3 eme parametre)
-	lea rsi, [rel buffer.buf]	; Mettre le ptr sur le buffer dans le 2 eme param
+_ft_cat:
+	push	rbp
+	mov		rbp,	rsp
+
+init:
+	mov		rdx,	SIZE
+	lea		rsi,	[rel buffer.buf]
 
 cat:
-	push rdi ; Envoi de fd
-	push rdx ; Envoi de size
-	push rsi ; Envoi de ptr
+	push	rdi
+	push	rdx
+	push	rsi
 
-	mov rax, MACH_SYSCALL(READ)
+	mov		rax, 	MACH_SYSCALL(READ)
 	syscall
-	jc error
+	jc 	error
 
-	mov rdi, STDOUT
-	mov rdx, rax ; Mettre la size lu dans la size a afficher
-	pop rsi ; Mettre ptr sur str
+	mov		rdi,	STDOUT
+	mov		rdx,	rax
+	pop		rsi
 
-	push rsi ; Envoi ptr sur str
-	push rax ; Envoi nb charactere read
+	push 	rsi
+	push 	rax
 
-	mov rax, MACH_SYSCALL(WRITE)
+	mov		rax,	MACH_SYSCALL(WRITE)
 	syscall
-	jc error
+	jc	error
 
-	pop rax
-	pop rsi
-	pop rdx
-	pop rdi
+	pop		rax
+	pop		rsi
+	pop		rdx
+	pop		rdi
 
-	cmp rax, 0 ; Tant que Read ne retourne pas zero continuer la boucle
-	jg cat
+	cmp		rax,	0
+	jg	cat
 
 done:
-	mov rsp, rbp
-	pop rbp
+	mov		rsp,	rbp
+	pop		rbp
 	ret
 
 error:
-	mov rax, -1
-	jmp done
+	mov		rax,	-1
+	jmp		done
+
+
 
 ;%define	MACH_SYSCALL(nb)	0x2000000 | nb
 ;%define READ				3
